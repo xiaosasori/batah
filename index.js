9571de816618aaf413ad48ec85e1b3be5d016bee
xiaosasori <xiaosasori@gmail.com>
2019-08-06 20:01:53 +0700

    as

--- index.js
+++ index.js
@@ -71,6 +71,6 @@ const server = new ApolloServer({
   }
 })
 // Run
-server.listen(process.env.PORT).then(({ url }) => {
+server.listen(process.env.PORT || 3000).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
